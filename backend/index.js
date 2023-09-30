3bfd5a77189eab6578aa98687d0bb4449903e963
himanshu <himanshusingh3179@gmail.com>
2023-09-30 19:19:05 +0530

    route user

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-}
-);
+});
+app.get('/user', (req, res) => {
+    res.send('you are on users page');
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
