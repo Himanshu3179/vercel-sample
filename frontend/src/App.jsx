0d1314bfe51360f4b6282f2cd0e64b424736f25f
himanshu <himanshusingh3179@gmail.com>
2023-09-30 19:32:19 +0530

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 
@@ -19,23 +19,36 @@ function App() {
 export default App;
 
 function Home() {
+  const [data, setData] = useState("")
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get("http://localhost:4000/")
+      // const response = await axios.get("http://localhost:4000/")
+      const response = await axios.get("https://vercel-sample-one-pied.vercel.app/")
       console.log(response)
+      setData(response.data)
     }
     fetchData()
   }
     , []);
   return <div>
-    from home
+    {data}
   </div>;
 }
 
 
 function Users() {
+  const [data, setData] = useState("")
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("https://vercel-sample-one-pied.vercel.app/user")
+      console.log(response)
+      setData(response.data)
+    }
+    fetchData()
+  }
+    , []);
   return <div>
-    from users
+    {data}
   </div>;
 }
 
