c081fbe82864ccc3676dab60ef919b8fee580f21
himanshu <himanshusingh3179@gmail.com>
2023-09-30 19:28:35 +0530

    cors and frontend conf

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
-  return <div>Hello World</div>;
+
+
+
+  return <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<Users />} />
+    </Routes>
+  </div>;
 }
 
 export default App;
+
+function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:4000/")
+      console.log(response)
+    }
+    fetchData()
+  }
+    , []);
+  return <div>
+    from home
+  </div>;
+}
+
+
+function Users() {
+  return <div>
+    from users
+  </div>;
+}
+
+
